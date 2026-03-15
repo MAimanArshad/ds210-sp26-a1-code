7a0500446d8b4fcf41fbd9ecf3b5bc7de24d34cf
Mohammad Aiman Arshad <mohammadaiman06.20@gmail.com>
2026-03-15 17:06:14 -0400

    Fully implemented v3

--- project_2_chatbot/basic_chatbot/src/solution/v3.rs
+++ project_2_chatbot/basic_chatbot/src/solution/v3.rs
@@ -4,34 +4,45 @@ use std::collections::HashMap;
 #[allow(dead_code)]
 pub struct ChatbotV3 {
     model: Llama,
-    user: HashMap<String, Chat<Llama>>,
+    users: HashMap<String, Chat<Llama>>,
 }
 
 impl ChatbotV3 {
     #[allow(dead_code)]
     pub fn new(model: Llama) -> ChatbotV3 {
-        let user = HashMap::new();
+        let users: HashMap<String, Chat<Llama>> = HashMap::new();
         return ChatbotV3 {
             model,
-            user,
+            users,
         };
     }
 
     #[allow(dead_code)]
     pub async fn chat_with_user(&mut self, username: String, message: String) -> String {
-        // Add your code for chatting with the agent while keeping conversation history here.
-        // Notice, you are given both the `message` and also the `username`.
-        // Use this information to select the correct chat session for that user and keep it
-        // separated from the sessions of other users.
-        return String::from("Hello, I am not a bot (yet)!");
+        if !self.users.contains_key(&username){
+            let chat_session: Chat<Llama> = self.model.chat();
+            self.users.insert(username.clone(), chat_session);
+        }
+        let output = self.users.get_mut(&username).unwrap().add_message(message).await;
+        match output {
+            Ok(response) => response,
+            Err(e) => format!("Error: {}", e),
+        }
     }
 
     #[allow(dead_code)]
     pub fn get_history(&self, username: String) -> Vec<String> {
-        // Extract the chat message history for the given username
-        // Hint: think of how you can retrieve the Chat object for that user, when you retrieve it
-        // you may want to use https://docs.rs/kalosm/0.4.0/kalosm/language/struct.Chat.html#method.session
-        // to then retrieve the history!
-        return Vec::new();
+        if let Some(chat) = self.users.get(&username){
+            let session = chat.session().unwrap();
+            let history = session.history();
+            let mut result = Vec::new();
+            for msg in history {
+                result.push(msg.content().to_string());
+            }
+            return result;
+        }
+        else{
+            return Vec::new();
+        }
     }
 }
