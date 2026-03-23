6bc80b471468870b253e9f3354436ac76be2b7a4
Mohammad Aiman Arshad <mohammadaiman06.20@gmail.com>
2026-03-22 20:25:18 -0400

    Fixed bugs in v4. Experiment now runs successfully.

--- project_2_chatbot/file_chatbot/src/solution/v4.rs
+++ project_2_chatbot/file_chatbot/src/solution/v4.rs
@@ -13,49 +13,58 @@ impl ChatbotV4 {
     }
 
     pub async fn chat_with_user(&mut self, username: String, message: String) -> String {
-        let filename = &format!("{}.txt", username);
+    let filename = &format!("{}.txt", username);
 
-        let mut chat_session: Chat<Llama> = self.model
-            .chat()
-            .with_system_prompt("The assistant will act like a pirate");
+    let mut chat_session: Chat<Llama> = self.model
+        .chat()
+        .with_system_prompt("The assistant will act like a pirate");
 
-        let history = file_library::load_chat_session_from_file(filename);
-        match history{
-            Some(h) => {
-                let mut continued_session = chat_session.with_session(h);
-                let output = continued_session.add_message(message).await;
-                match output{
-                    Ok(response) => response,
-                    Err(e) => format!("Error: {}", e),
-                }
-            }
-            None =>{
-                let output = chat_session.add_message(message).await;
-                match output{
-                    Ok(response) => response,
-                    Err(e) => format!("Error: {}", e),
-                }
+    let history = file_library::load_chat_session_from_file(filename);
+    match history{
+        Some(h) => {
+            let mut continued_session = chat_session.with_session(h);
+            let output = continued_session.add_message(message).await;
+            match output{
+                Ok(response) => {
+                    file_library::save_chat_session_to_file(filename,&continued_session.session().unwrap());
+                    return response;
+                },
+                Err(e) => format!("Error: {}", e),
             }
         }
-        // TODO: You have to implement the rest:
-        // You need to load the chat session from the file using file_library::load_chat_session_from_file(...).
-        // Think about what needs to happen if the function returns None vs Some(session).
-        // Hint: look at https://docs.rs/kalosm/latest/kalosm/language/struct.Chat.html#method.with_session
+        None =>{
+            let output = chat_session.add_message(message).await;
+            match output{
+                Ok(response) => {
+                    file_library::save_chat_session_to_file(filename,&chat_session.session().unwrap());
+                    return response;
+                },
+                Err(e) => format!("Error: {}", e),
+            }
+        }
     }
+}
 
 
 
 
     pub fn get_history(&self, username: String) -> Vec<String> {
-        let filename = &format!("{}.txt", username);
+    let filename = &format!("{}.txt", username);
 
-        match file_library::load_chat_session_from_file(&filename) {
-            None => {
-                return Vec::new();
-            },
-            Some(session) => {
-                return session.history().iter().map(|msg| msg.content().to_string()).collect();
-            }
+    match file_library::load_chat_session_from_file(&filename) {
+        None => {
+            return Vec::new();
+        },
+        Some(session) => {
+            return session
+                .history()
+                .iter()
+                .filter(|msg| {
+                    matches!(msg.role(), MessageType::UserMessage | MessageType::ModelAnswer)
+                })
+                .map(|msg| msg.content().to_string())
+                .collect();
         }
     }
+}
 }
