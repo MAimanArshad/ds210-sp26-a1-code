7781f0deb7a5dc15f04b9cd8e1ab288d951793e9
Devin Lu <qiqilu@bu.edu>
2026-04-12 17:03:55 -0400

    combined code for merging

--- project_4_tic_tac_toe/tic_tac_toe_3x3/src/solution/agent.rs
+++ project_4_tic_tac_toe/tic_tac_toe_3x3/src/solution/agent.rs
@@ -1,5 +1,5 @@
 use tic_tac_toe_stencil::agents::Agent;
-use tic_tac_toe_stencil::board::{self, Board};
+use tic_tac_toe_stencil::board::Board;
 use tic_tac_toe_stencil::player::Player;
 
 // Your solution solution.
@@ -10,7 +10,13 @@ impl Agent for SolutionAgent {
     // Should returns (<score>, <x>, <y>)
     // where <score> is your estimate for the score of the game
     // and <x>, <y> are the position of the move your solution will make.
-    fn solve(board: &mut Board, player: Player) -> (i32, usize, usize) {
+    fn solve(board: &mut Board, player: Player, _time_limit: u64) -> (i32, usize, usize) {
+        // If you want to make a recursive call to this solution, use
+        // `SolutionAgent::solve(...)`
+        if board.game_over() {
+            return (board.score(), 0, 0);
+        }
+        else {
             let moves = board.moves();
             let mut exp_board = board.clone();
             let mut scored: Vec<(i32, usize, usize)> = vec![];
@@ -19,6 +25,12 @@ impl Agent for SolutionAgent {
                 let score: (i32, usize, usize) = SolutionAgent::solve(&mut exp_board, player.flip());
                 exp_board.undo_move(step, player);
                 scored.push((score.0, step.0, step.1));
+
+            if player == Player::X {
+                return scored.into_iter().max_by_key(|x|, x.0).unwrap();
+        }
+            else {
+                return scored.into_iter().min_by_key(|x|, x.0).unwrap();
             }
         }
     }
