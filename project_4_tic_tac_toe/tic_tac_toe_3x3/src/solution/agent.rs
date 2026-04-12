8a07aaf2aaf9ec7cb8e80d1095157f1acd1b239a
Mohammad Aiman Arshad <mohammadaiman06.20@gmail.com>
2026-04-12 16:35:46 -0400

    Implemented loop part of recursion

--- project_4_tic_tac_toe/tic_tac_toe_3x3/src/solution/agent.rs
+++ project_4_tic_tac_toe/tic_tac_toe_3x3/src/solution/agent.rs
@@ -1,5 +1,5 @@
 use tic_tac_toe_stencil::agents::Agent;
-use tic_tac_toe_stencil::board::Board;
+use tic_tac_toe_stencil::board::{self, Board};
 use tic_tac_toe_stencil::player::Player;
 
 // Your solution solution.
@@ -11,8 +11,15 @@ impl Agent for SolutionAgent {
     // where <score> is your estimate for the score of the game
     // and <x>, <y> are the position of the move your solution will make.
     fn solve(board: &mut Board, player: Player) -> (i32, usize, usize) {
-        // If you want to make a recursive call to this solution, use
-        // `SolutionAgent::solve(...)`
-        unimplemented!("Not yet implemented")
+            let moves = board.moves();
+            let mut exp_board = board.clone();
+            let mut scored: Vec<(i32, usize, usize)> = vec![];
+            for step in moves{
+                exp_board.apply_move(step, player);
+                let score: (i32, usize, usize) = SolutionAgent::solve(&mut exp_board, player.flip());
+                exp_board.undo_move(step, player);
+                scored.push((score.0, step.0, step.1));
+            }
+        }
     }
 }
