6abc4c2006bd1e087a9b99c65f32e77b1f50ed2f
Devin Lu <qiqilu@bu.edu>
2026-04-19 00:53:07 -0400

    modified heuristic_evaluation

--- project_4_tic_tac_toe/tic_tac_toe_5x5/src/solution/agent.rs
+++ project_4_tic_tac_toe/tic_tac_toe_5x5/src/solution/agent.rs
@@ -10,29 +10,4 @@ impl Agent for SolutionAgent {
     // Should returns (<score>, <x>, <y>)
     // where <score> is your estimate for the score of the game
     // and <x>, <y> are the position of the move your solution will make.
-    fn solve(board: &mut Board, player: Player, _time_limit: u64) -> (i32, usize, usize) {
-        if board.game_over() {
-            return (board.score(), 0, 0);
-            }
-        else {
-            let moves = board.moves();
-            let mut scored: Vec<(i32, usize, usize)> = vec![];
-            //let mut exp_board = board.clone();
-            for step in moves{
-                board.apply_move(step, player);
-                let score = SolutionAgent::solve(board, player.flip(), _time_limit);
-                //let score = SolutionAgent::solve(&mut exp_board, player.flip(), _time_limit);
-                board.undo_move(step, player);
-                //exp_board.undo_move(step, player);
-                scored.push((score.0, step.0, step.1));
-            }
-            if player == Player::X {
-                return scored.into_iter().max_by_key(|x| x.0).unwrap();
-            }
-            else {
-                return scored.into_iter().min_by_key(|x| x.0).unwrap();
-            }
-        }
-    }
-}
-
+    
