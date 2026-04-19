fa285dd616539851b5d0248bad809a5e1e49dbaa
Devin Lu <qiqilu@bu.edu>
2026-04-19 01:00:58 -0400

    Implemented minimax

--- project_4_tic_tac_toe/tic_tac_toe_5x5/src/solution/agent.rs
+++ project_4_tic_tac_toe/tic_tac_toe_5x5/src/solution/agent.rs
@@ -11,6 +11,45 @@ impl Agent for SolutionAgent {
     // where <score> is your estimate for the score of the game
     // and <x>, <y> are the position of the move your solution will make.
     fn solve(board: &mut Board, player: Player, _time_limit: u64) -> (i32, usize, usize) {
-        unimplemented!("Not yet implemented")
-    }
+       let depth = 3;
+       minimax(board, player, depth)
+   }
+}
+
+
+fn minimax(board: &mut Board, player: Player, depth: usize) -> (i32, usize, usize) {
+   if board.game_over() {
+       return (board.score(), 0, 0);
+   }
+   if depth == 0 {
+       return (board.heuristic_evaluation(), 0, 0);
+   }
+   let moves = board.moves();
+   if moves.is_empty() {
+       return (board.heuristic_evaluation(), 0, 0);
+   }
+   let mut best_move = moves[0];
+   board.apply_move(best_move, player);
+   let mut best_score = minimax(board, player.flip(), depth - 1).0;
+   board.undo_move(best_move, player);
+  
+   for i in 1..moves.len() {
+       let m = moves[i];
+       board.apply_move(m, player);
+       let score = minimax(board, player.flip(), depth - 1).0;
+       board.undo_move(m, player);
+       if player == Player::X {
+           if score > best_score {
+               best_score = score;
+               best_move = m;
+           }
+       }
+       else {
+           if score < best_score {
+               best_score = score;
+               best_move = m;
+           }
+       }
+   }
+   return (best_score, best_move.0, best_move.1);
 }
